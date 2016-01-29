ed553bc3fa97d9da1b0b0c381873bf416aa6b416
Flavio Mazzolini <Flavio Mazzolini>
2016-01-29 22:54:45 +0100

    cartella

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private int vita;
+    private int velocità;
+    private int danno;
+
+    public int Vita
+    {
+        get { return vita; }
+        set { vita = value; }
+    }
+
+    public int Velocità
+    {
+        get { return velocità; }
+        set { velocità = value; }
+    }
+    public int Danno
+    {
+        get { return danno; }
+        set { danno = value; }
+    }
+
+    public void 
+}
