9bb35772f1b6bb663d1946a6b40de1f065fce6d6
Giacomo Franchini <Giacomo Franchini>
2016-01-30 08:43:19 +0100

    Menu di pausa in man scene

---
+++ Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pausemenu : MonoBehaviour {
+
+    public Canvas Gotomainm;
+    public Canvas Optionmenu;
+    public Canvas Buttoncanvas;
+    public Button Resumebutton;
+    public Button Optionbutton;
+    public Button Mainbutton;
+
+	// Use this for initialization
+	void Start () {
+
+        Gotomainm = Gotomainm.GetComponent<Canvas>();
+        Buttoncanvas = Buttoncanvas.GetComponent<Canvas>();
+        Optionmenu = Optionmenu.GetComponent<Canvas>();
+        Resumebutton = Resumebutton.GetComponent<Button>();
+        Optionbutton = Optionbutton.GetComponent<Button>();
+        Mainbutton = Mainbutton.GetComponent<Button>();
+
+        Gotomainm.gameObject.SetActive(false);
+        Optionmenu.gameObject.SetActive(false);
+	
+	}
+
+    public void Resumepress()
+    {
+        //ritrno al gioco
+    }
+	
+    public void Optionpress ()
+    {
+        Optionmenu.gameObject.SetActive(true);
+        Buttoncanvas.gameObject.SetActive(false);
+    }
+
+    public void Mainpress ()
+    {
+        Gotomainm.gameObject.SetActive(true);
+        Resumebutton.gameObject.SetActive(false);
+        Optionbutton.gameObject.SetActive(false);
+        Mainbutton.gameObject.SetActive(false);
+    }
+
+    public void Yespress ()
+    {
+        //lancio la scena del main menu
+    }
+
+    public void backpress ()
+    {
+        Gotomainm.gameObject.SetActive(false);
+        Optionmenu.gameObject.SetActive(false);
+        Resumebutton.gameObject.SetActive(true);
+        Optionbutton.gameObject.SetActive(true);
+        Mainbutton.gameObject.SetActive(true);
+    }
+
+    
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
