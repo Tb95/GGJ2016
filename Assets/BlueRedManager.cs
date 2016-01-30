323ad109f7a40f013fb5d4e26fe5a0db78043874
Fabio Chiusano <fabio.chiusano@epfl.ch>
2016-01-30 09:14:56 +0100

    first movement to red and blue

---
+++ Assets/BlueRedManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueRedManager : MonoBehaviour {
+	public int beginSection2 = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > beginSection2) {
+			transform.Rotate (0, 0.5f, 0);
+		}
+	}
+}
