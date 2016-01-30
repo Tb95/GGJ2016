8eacba4518d6cecf28d68f570959d5b3b600779b
Fabio Chiusano <fabio.chiusano@epfl.ch>
2016-01-30 09:45:11 +0100

    bluered together forever

--- Assets/Scripts/BlueRedManager.cs
+++ Assets/Scripts/BlueRedManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class BlueRedManager : MonoBehaviour {
 	public int beginSection2 = 10;
+	public int beginSection3 = 20;
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +12,10 @@ public class BlueRedManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > beginSection2) {
+		if (Time.time > beginSection2 && Time.time <= beginSection3) {
 			transform.Rotate (0, 0.5f, 0);
+		} else if (Time.time > beginSection3) {
+			transform.Rotate (0,  Mathf.Abs (Mathf.Sin (Time.time)), 0);
 		}
 	}
 }
