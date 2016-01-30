eb6cb619b33fcd70ba4e8249a89cb27c0592f268
Fabio Chiusano <fabio.chiusano@epfl.ch>
2016-01-30 01:12:13 +0100

    spiders should work now

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -8,7 +8,7 @@ public class Spider : MonoBehaviour {
 	public Movement movement = Movement.chase;
 	public int speed;
 	GameObject player;
-	/*
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -29,36 +29,36 @@ public class Spider : MonoBehaviour {
 		transform.LookAt (player.transform.position);
 	}
 
-	int deltaXForChaseZigZag = 0;
+	float deltaXForChaseZigZag = 0;
 
 	void chaseZigZag(GameObject player) {
-		if (deltaXForChaseZigZag <= 0.3) {
-			deltaXForChaseZigZag = Random.Range (3, 7);
+		if ((deltaXForChaseZigZag <= 0.3) && (deltaXForChaseZigZag >= -0.3)) {
+			deltaXForChaseZigZag = (float)(Random.Range (3, 7));
 			if (Random.Range (0, 2) == 0) { // Once every two
 				deltaXForChaseZigZag = -deltaXForChaseZigZag;
 			}
 		}
-		int tempDeltaX;
+		float tempDeltaX;
 		if (deltaXForChaseZigZag > 0) {
 			tempDeltaX = speed * Time.deltaTime;
 		} else {
 			tempDeltaX = (-speed) * Time.deltaTime;
 		}
-		transform.position = (player.transform.position - transform.position).normalized * speed * Time.deltaTime + tempDeltaX;
+		transform.position = ((player.transform.position - transform.position).normalized + (player.transform.right * tempDeltaX).normalized).normalized * speed * Time.deltaTime;
 		transform.LookAt (player.transform.position);
 	}
 
-	int deltaXForChaseWandering = 0;
+	float deltaXForChaseWandering = 0;
 	int directionForChaseWandering = 0;
 
 	void chaseWandering(GameObject player) {
 		if ((deltaXForChaseWandering <= 0.3) && (Random.Range (0, 80) == 0)) {
-			deltaXForChaseWandering = Random.Range (3, 7);
+			deltaXForChaseWandering = (float)(Random.Range (3, 7));
 			directionForChaseWandering = Random.Range (0, 2);
 		}
 		if (deltaXForChaseWandering > 0.3) {
 			if (directionForChaseWandering == 0) {
-				transorm.position += transform.right * speed * Time.deltaTime;
+				transform.position += transform.right * speed * Time.deltaTime;
 				deltaXForChaseWandering -= speed * Time.deltaTime;
 			} else {
 				transform.position += transform.right * (-1) * speed * Time.deltaTime;
@@ -68,5 +68,5 @@ public class Spider : MonoBehaviour {
 			transform.position = (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
 		}
 		transform.LookAt (player.transform.position);
-	}*/
+	}
 }
