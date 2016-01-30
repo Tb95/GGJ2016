fb932993f7fc323a276b823ac8472b6974a58a61
Tommaso Bianchi <tommaso.bianchi@epfl.ch>
2016-01-30 08:59:28 +0100

    Set music volume sliders

---
+++ Assets/Scripts/OptionMusic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class OptionMusic : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+
+    public void ChangeMusicVolume(Slider slider)
+    {
+        mainMixer.SetFloat("musicVol", slider.value);
+    }
+
+    public void ChangeSFXVolume(Slider slider)
+    {
+        mainMixer.SetFloat("sfxVol", slider.value);
+    }
+}
